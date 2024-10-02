5a9b5decf93461ef1d5db9027d0bc1aadf543c58
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-10-02 10:38:01 +0600

    Part 1, Model class, viewmodel, repository

---
+++ app/src/main/java/org/freedu/fcmapp/viewmodel/NotificationViewModel.kt
@@ -0,0 +1,33 @@
+package org.freedu.fcmapp.viewmodel
+
+import android.icu.text.CaseMap.Title
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.freedu.fcmapp.repository.FCMRepository
+
+class NotificationViewModel(private val fcmRepository: FCMRepository):ViewModel() {
+    private val _notificationSent = MutableLiveData<Boolean>()
+    val notificationSent : LiveData<Boolean> get() = _notificationSent
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage:LiveData<String> get() = _errorMessage
+
+
+    fun sendNotification(title: String, body:String){
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                fcmRepository.sendNotification(title, body,{
+                    _notificationSent.postValue(true)
+                },{
+                    _errorMessage.postValue(it.message?:"Unknown Error")
+                })
+            }catch (e:Exception){
+                _errorMessage.postValue(e.message?:"Unexpected Error")
+            }
+        }
+    }
+}
